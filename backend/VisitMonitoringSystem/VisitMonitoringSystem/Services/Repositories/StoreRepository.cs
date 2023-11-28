c4c283af9d36fcc62d5b520d6e184c49b0930d40
hyades91 <andrasfustos91@gmail.com>
2023-11-29 00:08:19 +0100

    Add StoreRepository

---
+++ backend/VisitMonitoringSystem/VisitMonitoringSystem/Services/Repositories/StoreRepository.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using NPOI.SS.Formula.Functions;
+using VisitMonitoringSystem.Models;
+using Newtonsoft.Json;
+
+namespace VisitMonitoringSystem.Services.Repositories;
+
+public class StoreRepository:IStoreRepository
+{
+    private VmsContext _dbContext;
+    public StoreRepository(VmsContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public IEnumerable<Store> GetAllStores()
+    {
+        var AllStores = _dbContext.Stores;
+        return AllStores;
+    }
+
+    public IEnumerable<Store> GetActiveStores()
+    {
+        var AllStores = _dbContext.Stores.Where(s=>s.Active==true);
+        return AllStores;
+    }
+
+
+    public IEnumerable<Store> DeleteAllStores()
+    {
+        var AllStores = _dbContext.Stores;
+        _dbContext.RemoveRange(AllStores);
+        _dbContext.SaveChanges();
+        return AllStores;
+    }
+
+    public  IEnumerable<Store> SetToActivOrDeactiv(int StoreNumber)
+    {
+
+        var selectedStore = _dbContext.Stores.FirstOrDefault(store => store.StoreNumber == StoreNumber);
+            {
+                selectedStore.Active=(!selectedStore.Active);
+                _dbContext.SaveChanges();
+            }
+            return _dbContext.Stores;
+
+    }
+
+    public IEnumerable<Store> UpdateRisks()
+    {
+        throw new NotImplementedException();
+    }
+}
